4b84ae4339ba20849a4a2d51b7b3e2d33f47812a
tgraceleaselink <tgrace@leaselink.ai>
2025-07-04 07:56:43 -0500

    Update app.py
    Updated Filter Types

--- app.py
+++ app.py
@@ -138,6 +138,13 @@ async def tenant_send_message(request: Request, authorization: Optional[str]=Hea
     if auth["sub"] != auth_id:
         raise HTTPException(status_code=403, detail="auth_id does not match token")
     try:
+        filtertype = ""
+        if entity_type == "tenant":
+            filtertype = "tenantid"
+        else if entity_type == "property":
+            filtertype = "propertyid"
+        else if entity_type == "unit":
+            filtertype = "unitid"
         oldmessages = Supabase_api.message_get_request(supabase_client, session_id, "entity_questions")
         final_message,  prompt_tokens, prompt_cost, completion_tokens, completion_cost, json_data = Qdrant_ChatGPT.get_relevant_chunks(collectionName, qdrant_client, "entity_id", entity_id, company_id, message, OpenAIclient, oldmessages, supabase_client)
         if final_message != None:
