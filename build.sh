16e29e80f71b2da686b51af54c0f38dd181e7c45
tgraceleaselink <tgrace@leaselink.ai>
2025-07-13 21:18:54 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 🚀 Update system packages and install Tesseract
-apt-get update && apt-get install -y tesseract-ocr
-
-# 🐍 Install Python dependencies
-pip install -r requirements.txt
