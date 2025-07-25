8382c7b7b44e18c811c74d664972e8c90b761a35
tjgrace7 <121319122+tjgrace7@users.noreply.github.com>
2025-07-25 16:58:29 -0500

    Fixed syntax error in upload_lease_manager line 48. passed status later

--- worker_service/upload_lease_manager.py
+++ worker_service/upload_lease_manager.py
@@ -45,7 +45,7 @@ def load_pdf(auth_id, propertyid, unit_id, tenantid, get_pdf, lease_id, bucket_n
         lease_data['page_count'] = total_pages
         #upserts lease_data into lease_documents table in supabase
         Supabase_api.supabase_post_request(supabase_client, [lease_data], "lease_documents")
-        job_status[job_id]['status'] = 'done'
+        job_status['status'] = 'done'
         supabase_client.table('Upload_Job_Status').update({'job_info': job_status}).eq('job_id', job_id)
 
         
