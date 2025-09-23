d2a0769a1ec0af8c4b3896cd77ec6a64ee0c5ecb
tgraceleaselink <tgrace@leaselink.ai>
2025-09-23 13:13:52 -0500

    Update Qdrant_ChatGPT.py

--- web_api/Qdrant_ChatGPT.py
+++ web_api/Qdrant_ChatGPT.py
@@ -102,7 +102,7 @@ Return a **single, semantically precise** version of the user's question that wi
         results = q_client.search(
             collection_name=collection_Name,
             query_vector=("dense-vector", message_vector),
-            limit=15,
+            limit=20,
             with_payload=True,
             with_vectors=False,
             query_filter=Filter(
